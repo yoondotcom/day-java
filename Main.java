5d89f565d11e71eca45ebb6a3bfbadfb9d8007d2
Kim Ji Yun <62929727+yoondotcom@users.noreply.github.com>
2022-08-28 21:02:43 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args){
+    String name;
+
+    Scanner sc = new Scanner(System.in);
+    name = sc.next();
+
+    System.out.println("Hello " + name);
+  }
+}
