a39ddb9676fecb61f849259c181f58911d367d59
kimjiyun <didany15@dongguk.ac.kr>
2022-09-27 21:53:55 +0900

    무한히 계산하기

--- Calculator.java
+++ Calculator.java
@@ -1,72 +1,75 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Calculator{
     public static void main(String[] args){
-
-        Calculate();
-
-    }
-
-    private static void Calculate() {
-        List<Integer> list = new ArrayList<>();
-
+        ArrayList<Integer> arrayList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        while(true){
-            String value = sc.nextLine();
+        int number = 0;
+        String sign = "";
 
-            if(value.equals("add")) {
-                System.out.println("Result is " + add(list));
-                break;
-            }else if(value.equals("min")) {
-                System.out.println("Result is " + min(list));
-                break;
-            }else if(value.equals("mul")) {
-                System.out.println("Result is " + mul(list));
-                break;
-            }else if(value.equals("div")) {
-                System.out.println("Result is " + div(list));
-                break;
-            }else{
-                list.add(Integer.parseInt(value));
-                list.add(Integer.parseInt(value));
+        while (true) {
+            if (sc.hasNextInt()) {
+                number = sc.nextInt();
+                arrayList.add(number);
+            } else {
+                sign = sc.next();
+                System.out.println("arrayList : " + arrayList);
+                System.out.println("sign : " + sign);
+                calculate(arrayList, sign);
+                arrayList.clear();
             }
         }
-        Calculate();
-
     }
 
-    public static int add(List a) {
+    private static void calculate(ArrayList<Integer> arrayList, String sign) {
+
+        if(sign.equals("add")) {
+            System.out.println("Add Result is " + add(arrayList));
+        }else if(sign.equals("min")) {
+            System.out.println("Min Result is " + min(arrayList));
+        }else if(sign.equals("mul")) {
+            System.out.println("Mul Result is " + mul(arrayList));
+        }else if(sign.equals("div")) {
+            System.out.println("Div Result is " + div(arrayList));
+        }
+    }
+
+    public static int add(ArrayList<Integer> arrayList) {
         int result = 0;
-        for(int i = 0; i > a.size(); i++){
-            int enter = (int) a.get(i);
-            result += enter;
+        for(int i = 0; i < arrayList.size(); i++){
+            result += arrayList.get(i);
         }
         return result;
     }
 
-    public static int min(List a) {
-        int result = 0;
-        for(int i = 0; i > a.size(); i++){
-            result -= i;
+    public static int min(ArrayList<Integer> arrayList) {
+        int result = arrayList.get(0);
+        arrayList.remove(0);
+        for(int i = 0; i < arrayList.size(); i++){
+            int enter = arrayList.get(i);
+            result -= enter;
         }
         return result;
     }
 
-    public static int mul(List a) {
-        int result = 0;
-        for(int i = 0; i > a.size(); i++){
-            result *= i;
+    public static int mul(ArrayList<Integer> arrayList) {
+        int result = arrayList.get(0);
+        arrayList.remove(0);
+        for(int i = 0; i < arrayList.size(); i++){
+            int enter = arrayList.get(i);
+            result *= enter;
         }
         return result;
     }
 
-    public static int div(List a) {
-        int result = 0;
-        for(int i = 0; i > a.size(); i++){
-            result /= i;
+    public static int div(ArrayList<Integer> arrayList) {
+        int result = arrayList.get(0);
+        arrayList.remove(0);
+        for(int i = 0; i < arrayList.size(); i++){
+            int enter = arrayList.get(i);
+            result /= enter;
         }
         return result;
     }
